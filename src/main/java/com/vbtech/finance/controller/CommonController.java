df8e2017dba2bf474a5239f67f1112414b5c2a41
VikasBharti1203 <bharati.vikas1203@gmail.com>
2023-09-12 01:44:53 +0530

    controller changes

--- src/main/java/com/vbtech/finance/controller/CommonController.java
+++ src/main/java/com/vbtech/finance/controller/CommonController.java
@@ -12,15 +12,22 @@ import com.vbtech.finance.repository.CustomerRepository;
 @RequestMapping("/customer")
 public class CommonController {
     @Autowired
-    private CustomerRepository studentRepo;
+    private CustomerRepository customerRepo;
+
+     @GetMapping("/status")
+   public String getCustomerCount(){
+        String msg ="Database Configuration Done..Connected to Postgres..Total Customer Count from DB : ";
+        int count = this.getAllCustomer().size();
+        return count>0 ? msg+0: msg+count;
+    }
 
     @GetMapping("/findAll")
-   public List<Customer> getAllStudent(){
-        return studentRepo.findAll();
+   public List<Customer> getAllCustomer(){
+        return customerRepo.findAll();
     }
 
     @PostMapping("/insert")
     public Customer insert(@RequestBody Customer cutomer){
-        return studentRepo.save(cutomer);
+        return customerRepo.save(cutomer);
     }
 }
