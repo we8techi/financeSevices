23eccdde1c0aca571a7a24dfe7437afbe6eb3c8e
Balaji.Murkute <balajimurkute2412@gmail.com>
2023-11-07 09:19:06 +0530

    Swagger Implementation added

---
+++ src/main/java/com/we8techi/platform/finance/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.we8techi.platform.finance.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.we8techi.platform.finance"))
+                .paths(PathSelectors.any())
+                .build().apiInfo(apiInfoMetaData());
+    }
+
+    private ApiInfo apiInfoMetaData() {
+
+        return new ApiInfoBuilder().title("Finance Services API Documentation")
+                .description("Finance Services swagger")
+                .contact(new Contact("Dev-Team", "https://reacttemplate.onrender.com/", "we8techstartup@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
+    }
+}
+
