712fc9dbd8c3022ebe36da64605470d6996608d5
jadhavd <jadhavd@Sysmex.com>
2024-04-11 15:25:28 +0530

    Customer file upload changes.

--- src/main/java/com/we8techi/platform/finance/service/CustomerService.java
+++ src/main/java/com/we8techi/platform/finance/service/CustomerService.java
@@ -2,7 +2,10 @@ package com.we8techi.platform.finance.service;
 
 import com.we8techi.platform.finance.objects.APIResponse;
 import com.we8techi.platform.finance.objects.CustomerDTO;
+import com.we8techi.platform.finance.objects.CustomerDocumentsDTO;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 public interface CustomerService {
@@ -44,4 +47,13 @@ public interface CustomerService {
      * @return
      */
     APIResponse deleteCustomerDetails(Long companyId, Long customerId);
+
+    /**
+     * Upload customer document.
+     * @param companyId
+     * @param customerId
+     * @param file
+     * @param customerDocumentsDTO
+     */
+    void uploadCustomerDocuments(Long companyId, Long customerId, MultipartFile file, CustomerDocumentsDTO customerDocumentsDTO) throws IOException;
 }
