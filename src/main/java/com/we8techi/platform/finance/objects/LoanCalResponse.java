6612d6d355a00aa3d56f652835f9217a2da0300f
we8techi <144616616+we8techi@users.noreply.github.com>
2024-10-07 01:25:07 +0530

    Update LoanCalResponse.java to resolve compilation issue

--- src/main/java/com/we8techi/platform/finance/objects/LoanCalResponse.java
+++ src/main/java/com/we8techi/platform/finance/objects/LoanCalResponse.java
@@ -2,16 +2,19 @@ package com.we8techi.platform.finance.objects;
 
 import java.time.LocalDate;
 
-public record LoanCalResponse(LocalDate returnDate, 
-                              Double perDayInterestAmount, 
-                              Double monthlyInterestAmount,
-                              Double issueAmount, 
-                              Double passbookCharges,
-                              LocalDate collectionFrom, 
-                              String loanType,
-                              Double totalCollectionAmount,
-                              Double dailyCollectionAmount,
-                              Double monthlyCollectionAmount) {
+public class LoanCalResponse {
 
+    LocalDate returnDate;
+    Double perDayInterestAmount;
+    Double monthlyInterestAmount;
+    Double issueAmount;
+    Double passbookCharges;
+    LocalDate collectionFrom;
+    String loanType;
+    Double totalCollectionAmount;
+    Double dailyCollectionAmount;
+    Double monthlyCollectionAmount;
 
+    public LoanCalResponse(LocalDate collectionStartDate, Double perDayInterest, Double thirtyDaysInterest, Double issueAmount, Double passbookCharge, LocalDate collectionStartDate1, String d, double v, Double perDayInterest1, double v1) {
+    }
 }
