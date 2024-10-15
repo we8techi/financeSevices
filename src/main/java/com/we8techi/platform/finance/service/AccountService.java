877491d088a85bc9d1e51587882201db4aad5ace
we8techi <144616616+we8techi@users.noreply.github.com>
2024-10-15 08:39:16 +0530

    Add files via upload

---
+++ src/main/java/com/we8techi/platform/finance/service/AccountService.java
@@ -0,0 +1,19 @@
+package com.we8techi.platform.finance.service;
+
+import com.we8techi.platform.finance.entity.Account;
+import com.we8techi.platform.finance.objects.APIResponse;
+
+import java.util.List;
+
+public interface AccountService {
+
+    Account createAccount(Account account);
+
+    List<Account> getAllAccountsForCustomer(Long customerId);
+
+    Account getAccount(Long account_id);
+
+    Account updateAccount(Long account_id,Account account);
+
+    APIResponse deleteAccount(Long account_id);
+}
