c716ac097d03182b8048ccf7eff4c433f3e656cb
we8techi <144616616+we8techi@users.noreply.github.com>
2024-10-15 08:41:02 +0530

    Update IncomeServiceImpl.java

--- src/main/java/com/we8techi/platform/finance/service/impl/IncomeServiceImpl.java
+++ src/main/java/com/we8techi/platform/finance/service/impl/IncomeServiceImpl.java
@@ -1,10 +1,13 @@
 package com.we8techi.platform.finance.service.impl;
 
+import com.we8techi.platform.finance.entity.Customer;
 import com.we8techi.platform.finance.entity.Income;
+import com.we8techi.platform.finance.mapper.CustomerMapper;
 import com.we8techi.platform.finance.mapper.IncomeMapper;
 import com.we8techi.platform.finance.objects.IncomeDTO;
 import com.we8techi.platform.finance.repository.IncomeRepository;
 import com.we8techi.platform.finance.service.IncomeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +17,7 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class IncomeServiceImpl implements IncomeService {
 
     @Autowired
@@ -23,26 +27,32 @@ public class IncomeServiceImpl implements IncomeService {
     private IncomeMapper incomeMapper;
 
     @Override
-    public IncomeDTO saveIncome(IncomeDTO incomeDTO) {
+    public IncomeDTO saveIncome(Long companyId,IncomeDTO incomeDTO) {
+        log.info("Add Income for a companyId={}", companyId);
         Income income = incomeMapper.toEntity(incomeDTO);
         Income savedIncome = incomeRepository.save(income);
+        log.info("Income added successfully.");
         return incomeMapper.toDto(savedIncome);
+
+        /*Income income = incomeMapper.toEntity(incomeDTO);
+        Income savedIncome = incomeRepository.save(income);
+        return incomeMapper.toDto(savedIncome);*/
     }
 
     @Override
-    public Optional<IncomeDTO> getIncomeById(Long id) {
+    public Optional<IncomeDTO> getIncomeById(Long companyId,Long id) {
         Optional<Income> income = incomeRepository.findById(id);
         return income.map(incomeMapper::toDto);
     }
 
     @Override
-    public List<IncomeDTO> getAllIncomes() {
+    public List<IncomeDTO> getAllIncomes(Long companyId) {
         List<Income> incomes = incomeRepository.findAll();
         return incomes.stream().map(incomeMapper::toDto).collect(Collectors.toList());
     }
 
     @Override
-    public IncomeDTO updateIncome(Long id, IncomeDTO incomeDTO) {
+    public IncomeDTO updateIncome(Long companyId,Long id, IncomeDTO incomeDTO) {
         return incomeRepository.findById(id).map(existingIncome -> {
             incomeMapper.toEntity(incomeDTO); // Update entity with DTO values
             existingIncome.setUpdatedAt(LocalDateTime.now());
@@ -52,9 +62,7 @@ public class IncomeServiceImpl implements IncomeService {
     }
 
     @Override
-    public void deleteIncome(Long id) {
+    public void deleteIncome(Long companyId,Long id) {
         incomeRepository.deleteById(id);
     }
 }
-
-
